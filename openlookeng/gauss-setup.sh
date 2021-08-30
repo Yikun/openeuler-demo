da60ee50e9a0471a7f38275acfb6411a1ec943c7
Yikun Jiang <yikunkero@gmail.com>
2021-08-30 20:00:06 +0800

    sync date load script.

--- openlookeng/gauss-setup.sh
+++ openlookeng/gauss-setup.sh
@@ -1,4 +1,4 @@
 docker rm -f opengauss
 docker run --name opengauss --privileged=true -d -e GS_PASSWORD=Pass@123 -p 8003:5432 enmotech/opengauss:2.0.1
 
-docker exec -u root -ti opengauss bash -c "apt-get update;apt install git -y;git clone https://github.com/vrajmohan/pgsql-sample-data.git ~/pgsql-sample-data;cd ~/pgsql-sample-data/employee;/entrypoint.sh gsql -d postgres -U gaussdb -W'Pass@123' -f employees.sql"
+docker exec -u root -ti opengauss bash -c "apt-get update;apt install git -y;git clone https://github.com/Yikun/openeuler-demo.git ~/openeuler-demo;cd ~/openeuler-demo/openlookeng/gauss;/entrypoint.sh gsql -d postgres -U gaussdb -W'Pass@123' -f employees.sql"
--- openlookeng/mysql-setup.sh
+++ openlookeng/mysql-setup.sh
@@ -1,4 +1,4 @@
 docker rm -f mysql
 docker run --name mysql -d --privileged=true -e MYSQL_ROOT_PASSWORD=Pass@123 -p 8002:3306 centos/mysql-80-centos7
 
-docker exec -u root -ti mysql bash -c "yum install git -y;git clone https://github.com/datacharmer/test_db.git;cd test_db;mysql -uroot -t < employees.sql"
+docker exec -u root -ti mysql bash -c "yum install git -y;git clone https://github.com/Yikun/openeuler-demo.git;cd ~/openeuler-demo/openlookeng/mysql;mysql -uroot -t < employees.sql"
